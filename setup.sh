1e437f8bdeb2df2ab91ded060ce86b6e27efc05c
Sascha Eisenmann <64353115+sascha-eisenmann@users.noreply.github.com>
2022-11-23 18:04:51 +0100

    Added line breaks to output

--- setup.sh
+++ setup.sh
@@ -7,21 +7,28 @@
 brew install mas
 
 # 3. Install basics tools from Brewfile
+echo ""
 echo "Installing basic tools"
+echo ""
 curl -fsSL https://raw.githubusercontent.com/pragtics/machine-setup-mac/master/basics.Brewfile | brew bundle --file=/dev/stdin
 
 # 4. Install development tools from Brewfile
 read -p "Do you want to install additional development tools? (y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	echo ""
     echo "Installing development tools"
     curl -fsSL https://raw.githubusercontent.com/pragtics/machine-setup-mac/master/development.Brewfile | brew bundle --file=/dev/stdin
 fi
 
+echo ""
+echo ""
+    
 # 5. Install personal tools from Brewfile
 read -p "\nDo you want to install additional personal tools from personal/$USER? (y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	echo ""
     echo "Installing personal tools"
     curl -fsSL "https://raw.githubusercontent.com/pragtics/machine-setup-mac/personal/$USER/personal.Brewfile" | brew bundle --file=/dev/stdin
 fi
